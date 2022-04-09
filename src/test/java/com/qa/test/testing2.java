3c49b119174c7802cfc4f81cf33e6809c42f4361
Suryadeep Srivastava <suryadeepigi@gmail.com>
2022-04-09 18:53:41 +0530

    new change added testing 2

---
+++ src/test/java/com/qa/test/testing2.java
@@ -0,0 +1,12 @@
+package com.qa.test;
+
+public class testing2 {
+
+	public static void main(String[] args) {
+		System.out.println("hello testing 2");
+	}
+
+	public void test2() {
+		System.out.println("hello hee=llo");
+	}
+}
