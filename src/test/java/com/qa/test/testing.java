5911994b4a787b234c22b69ad80d6456e241bacf
Suryadeep Srivastava <suryadeepigi@gmail.com>
2022-04-09 18:10:06 +0530

    i am trying to commit a demo repository

--- src/test/java/com/qa/test/testing.java
+++ src/test/java/com/qa/test/testing.java
@@ -1,10 +1,31 @@
 package com.qa.test;
 
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+
 public class testing {
 
-	public void testingfile() {
+	@Test(retryAnalyzer = com.analyzer.retryanalyzer.class)
+	public void tone() {
 		
-		System.out.println("testing file");
+		String a="a";
+		String b="b";
+		Assert.assertEquals(a,b);
 	}
 	
+	@Test(retryAnalyzer = com.analyzer.retryanalyzer.class)
+	public void ttwo() {
+		
+		String a="a";
+		String b="a";
+		Assert.assertEquals(a,b);
+	}
+	
+	@Test
+	public void tthree() {
+		String a="a";
+		String b = "b";
+		Assert.assertEquals(a,b);
+	}
 }
